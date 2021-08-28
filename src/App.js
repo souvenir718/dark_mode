00e529395ca530bbdb986c408ac36a081b3349e5
souvenir718 <mongkey0@naver.com>
2021-08-28 12:21:01 +0900

    [0828] ThemeProvider 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Main, Sub } from "./pages";
+import { GlobalStyle } from "./theme/GlobalStyle";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <ThemeProvider>
+                <GlobalStyle />
+                <Suspense fallback={<div>loading....</div>}>
+                    <Switch>
+                        <Route exact path="/" component={Main} />
+                        <Route exact path="/sub" component={Sub} />
+                    </Switch>
+                </Suspense>
+            </ThemeProvider>
+        </BrowserRouter>
+    );
 };
 
 export default App;
